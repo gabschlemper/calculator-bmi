a9371a3a54c09488f1af5f32d6bff10d25b36ba7
Gabriela Schlemper <gabschlemper@gmail.com>
2021-03-25 17:09:55 -0300

    add js

---
+++ bmi.js
@@ -0,0 +1,36 @@
+const calculate = document.getElementById('calculate');
+
+function bmi() {
+  const name = document.getElementById('name').value;
+  const height = document.getElementById('height').value;
+  const weight = document.getElementById('weight').value;
+  const result = document.getElementById('result');
+
+  if (name !== '' && height !== '' && weight !== '') {
+    
+      const valueBMI = (weight / (height * height)).toFixed(1);
+
+      let classification = "";
+
+      if (valueBMI < 18.5) {
+        classification = 'you are underweight, your risk of comorbidities is low.';
+      } else if (valueBMI < 25) {
+        classification = 'you are normal weight, your risk of comorbidities is average.';
+      } else if (valueBMI < 30) {
+        classification = 'you are overweight, your risk of comorbidities is increased.';
+      } else if (valueBMI < 35) {
+        classification = 'you are obesity class I, your risk of comorbidities is moderate.';
+      } else if (valueBMI < 40) {
+        classification = 'you are obesity class II, your risk of comorbidities is severe.';
+      } else if (valueBMI > 40) {
+        classification = 'you are obesity class III, your risk of comorbidities is very severe.';
+      } 
+
+      result.textContent = `${name} your BMI is ${valueBMI}, ${classification}`;
+
+  } else {
+    result.textContent = `Please, complete all fields! `
+  }
+}
+
+calculate.addEventListener('click', bmi);
